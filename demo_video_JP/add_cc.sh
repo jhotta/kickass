cfdfe122de423ff760bc680c5901bcc53f2e0435
Jay Hotta <fukuzo@cybertron.co.jp>
2017-05-16 15:59:31 +0900

    changed script file name

--- demo_video_JP/add_cc.sh
+++ demo_video_JP/add_cc.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-rm -rf vertical_demo_JP.mp4 && ffmpeg -i vertical_demo_two.mp4 -vf ass=vertical_demo_two_jp_test.ass vertical_demo_JP.mp4
+rm -rf vertical_demo_JP.mp4 && ffmpeg -i vertical_demo_two.mp4 -vf ass=vertical_demo_jp_final.ass vertical_demo_JP.mp4
